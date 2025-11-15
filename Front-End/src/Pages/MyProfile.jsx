51cdb1dfaaffd27c0742f630f5d57e7b6cae00f3
Aniket Rana <aniketrana7681@gmail.com>
2025-11-15 12:11:36 +0530

    add

--- Front-End/src/Pages/MyProfile.jsx
+++ Front-End/src/Pages/MyProfile.jsx
@@ -16,8 +16,118 @@ const MyProfile = () => {
   const [isEdit, setIsEdit] = useState(false);
   return (
     <div>
-      <img src={userData.image} alt="" onChange={(e) => setUserData()} />
-      {isEdit ? <input type="text" /> : <p>{userData.name}</p>}
+      <img src={userData.image} alt="" />
+      {isEdit ? (
+        <input
+          type="text"
+          value={userData.name}
+          onChange={(e) =>
+            setUserData((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+      ) : (
+        <p>{userData.name}</p>
+      )}
+      <hr />
+      <div>
+        <p>CONTACT INFORMATION</p>
+        <div>
+          <p>Email id:</p>
+          <p>{userData.email}</p>
+          <p>Phone:</p>
+          {isEdit ? (
+            <input
+              type="text"
+              value={userData.phone}
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, phone: e.target.value }))
+              }
+            />
+          ) : (
+            <p>{userData.phone}</p>
+          )}
+          <p>Address:</p>
+          {isEdit ? (
+            <p>
+              <input
+                type="text"
+                value={userData.address.line1}
+                onChange={(e) =>
+                  setUserData((prev) => ({
+                    ...prev,
+                    address: { ...prev.address, line1: e.target.value },
+                  }))
+                }
+              />
+              <br />
+              <input
+                type="text"
+                value={userData.address.line2}
+                onChange={(e) =>
+                  setUserData((prev) => ({
+                    ...prev,
+                    address: { ...prev.address, line2: e.target.value },
+                  }))
+                }
+              />
+            </p>
+          ) : (
+            <p>
+              {userData.address.line1}
+              <br />
+              {userData.address.line2}
+            </p>
+          )}
+        </div>
+      </div>
+      {/* Basic Information */}
+      <div>
+        <p>Basic Information</p>
+        <div>
+          <p>Gender:</p>
+          {isEdit ? (
+            <select
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, gender: e.target.value }))
+              }
+            >
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+          ) : (
+            <p>{userData.gender}</p>
+          )}
+          <p>Birthday:</p>
+          {isEdit ? (
+            <input
+              value={userData.dob}
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, dob: e.target.value }))
+              }
+              type="date"
+            ></input>
+          ) : (
+            <p>{userData.dob}</p>
+          )}
+        </div>
+      </div>
+      <div>
+        {isEdit ? (
+          <button
+            onClick={() => setIsEdit(false)}
+            className="border border-blue-400 m-2 py-1 px-4 rounded-xl"
+          >
+            Save information
+          </button>
+        ) : (
+          <button
+            onClick={() => setIsEdit(true)}
+            className="border border-blue-400 m-2 py-1 px-4 rounded-xl"
+          >
+            Edit
+          </button>
+        )}
+      </div>
     </div>
   );
 };
